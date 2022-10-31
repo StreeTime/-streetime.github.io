1933d1f0dbfd807e92a3a5ebd3f13e27a7a976df
s <s@pro.local>
2022-10-31 15:23:05 +0000

    first commit

---
+++ resources/s.js
@@ -0,0 +1,93 @@
+var term = ""; //global var
+
+function go(url,con,ext) {
+  window.open(url + term.slice(2).split(" ").join(con) + ext);
+  document.getElementById("search").focus().value(""); //clears, and returns focus to, the search bar
+}
+
+function bookmarkGo(url) {
+  url = term.split('|')[1];
+  window.open(url);
+  document.getElementById("search").focus().value(""); //clears, and returns focus to, the search bar
+}
+
+function research() {
+  var FirstLetter = term.charAt(0);
+  var SecondLetter = term.charAt(1);
+  var ThirdLetter = term.charAt(2);
+
+  if (FirstLetter == "[") {
+    bookmarkGo(term);
+    return;
+  };
+
+  switch (SecondLetter) {
+    case " ": //this is a search. find search engine from first letter
+      switch (FirstLetter) { //eg : go(root url,connector,extension)
+        case "a": go("https://www.amazon.co.uk/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=","+",""); break;
+        case "b": go("https://www.bing.com/search?q=","+",""); break;
+        case "c": go("https://www.desktopbackground.org/search?kwd=","",""); break;
+	case "i": go("https://www.bing.com/images/search?q=","+","&qft=+filterui:imagesize-custom_2560_1440"); break;
+        case "l": go("https://genius.com/search?q=","+",""); break;
+        case "n": go("https://search.nixos.org/packages?channel=22.05&from=0&size=50&sort=relevance&type=packages&query=","-",""); break;
+        case "r": go("https://libreddit.spike.codes/search?q=","",""); break;
+//        case "r": go("https://www.reddit.com/search?q=","+",""); break;
+        case "s": go("https://stackoverflow.com/search?q=","+",""); break; //do i want stack exchange??
+        case "t": go("https://translate.google.com/#auto/en/","+",""); break;
+        case "v": go("https://www.bing.com/videos/search?q=","+",""); break;
+        case "w": go("https://en.wikipedia.org/w/index.php?search="," ","&title=Special%3ASearch&fulltext=1&ns0=1"); break;
+        case "y": go("https://www.youtube.com/search?q=","+",""); break;
+//        case "y": go("https://invidious.snopyta.org/search?q=","",""); break;
+        case "4": go("https://boards.4chan.org/search#/","",""); break;
+      } //close switch
+    break; //case break
+    case "/": //this is a link (reddit or 4chan) find out which, go link
+      switch (FirstLetter) {
+        case "4": go("https://boards.4chan.org/","","/catalog"); break;
+        case "r":   if (term.indexOf(' ') >= 0) {
+                      var final = "https://www.reddit.com/" + term.substring(0, term.indexOf(" ")) + "/search/?q=" + term.substring(term.indexOf(" ") + 1) + "&restrict_sr=1&sr_nsfw=";
+                    } else {
+                      var final = "https://www.reddit.com/" + term;
+                    }
+                    window.open(final);
+                    break;
+        case "m":
+          switch (ThirdLetter) {
+            case "d": go("https://old.reddit.com/r/startpages+unixporn+FirefoxCSS+desktops+unixart+web_design","","",""); break;
+            case "m": go("https://old.reddit.com/r/konmari+declutter+Frugal+Minimalism+SimpleLiving+minimalist","","",""); break;
+            case "s": go("https://old.reddit.com/r/avporn+malelivingspace+roomporn+macsetups+desksetup+standingdesk+cozyplaces+OffGridCabins+CozyPlaces","","",""); break;
+            case "w": go("https://old.reddit.com/r/CityPorn+animewallpaper+offensive_wallpapers+wallpaper+wallpaperdump+wallpaperrequests+wallpapers+carporn","","",""); break;
+          }
+      }
+    break; //case break
+    default : //standard search
+      term = "aa" + term; //add two letters that will be removed in go
+      go("https://duckduckgo.com/?t=ffab&q=","+","");
+  }
+}
+
+function StageOne(enteredtext) {
+  term = enteredtext;
+
+  if (term.length == 0) {
+    return;
+  }
+  if (term.length == 1) {
+    switch (term) {
+      case "2": window.open("/resources/02.jpg"); break;
+      case "a": window.open("https://amazon.co.uk"); break;
+      case "b": window.open("http://192.168.1.254/"); break;
+      case "f": window.open("https://facebook.com"); break;
+      case "i": window.open("https://images.google.co.uk"); break;
+      case "l": window.open("https://genius.com"); break;
+      case "p": window.open("http://192.168.1.247/admin/"); break;
+      case "r": window.open("https://www.reddit.com/r/all/"); break;
+      case "t": window.open("https://twitter.com"); break;
+      case "w": window.open("https://wikipedia.org"); break;
+      case "y": window.open("https://youtube.com"); break;
+    }
+  }
+  if (term.length > 1) {
+      research();
+  }
+}
